7f71d3414098d0f3642797c6b12a9ceefa4b524f
Yusuke Yamasaki <yamasaki.dev0314@gmail.com>
2023-05-09 20:16:11 +0900

    feat: basic logic gate implementation based on nand.

--- src/common/gate.rs
+++ src/common/gate.rs
@@ -1,3 +1,27 @@
 pub fn nand(a: bool, b: bool) -> bool {
     !(a & b)
 }
+
+pub fn not(a: bool) -> bool {
+    nand(a, a)
+}
+
+pub fn and(a: bool, b: bool) -> bool {
+    not(nand(a, b))
+}
+
+pub fn or(a: bool, b: bool) -> bool {
+    nand(nand(a, a), nand(b, b))
+}
+
+pub fn xor(a: bool, b: bool) -> bool {
+    nand(nand(not(a), b), nand(a, not(b)))
+}
+
+pub fn mux(a: bool, b: bool, sel: bool) -> bool {
+    or(and(a, not(sel)), and(b, sel))
+}
+
+pub fn dmux(input: bool, sel: bool) -> (bool, bool) {
+    (and(input, not(sel)), and(input, sel))
+}
