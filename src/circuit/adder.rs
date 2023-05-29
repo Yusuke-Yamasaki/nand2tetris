65e41fe4559fce2e9ddf9be5a52a4c7e0f9e05ee
Yusuke Yamasaki <yamasaki.dev0314@gmail.com>
2023-05-29 21:28:25 +0900

    feat: full-adder

--- src/circuit/adder.rs
+++ src/circuit/adder.rs
@@ -7,3 +7,10 @@ pub fn half_adder(a: bool, b: bool) -> [bool; 2] {
         and(a, b), // carry
     ]
 }
+
+pub fn full_adder(a: bool, b: bool, c: bool) -> [bool; 2] {
+    [
+        half_adder(half_adder(a, b)[0], c)[0], // sum
+        or(half_adder(half_adder(a, b)[0], c)[1], half_adder(a, b)[1]), // carry
+    ]
+}
