9f889f386c9f228ffb825438ae3c4abb47bfb708
Yusuke Yamasaki <yamasaki.dev0314@gmail.com>
2023-05-29 21:20:02 +0900

    refac: dmux return [bool; 2] instead of (bool, bool).

--- src/circuit/combinational_logic.rs
+++ src/circuit/combinational_logic.rs
@@ -1,11 +1,13 @@
 use super::basic_logic::and;
 use super::basic_logic::not;
 use super::basic_logic::or;
+use super::basic_logic::Word;
 
 pub fn mux(a: bool, b: bool, sel: bool) -> bool {
     or(and(a, not(sel)), and(b, sel))
 }
 
-pub fn dmux(input: bool, sel: bool) -> (bool, bool) {
-    (and(input, not(sel)), and(input, sel))
+pub fn dmux(input: bool, sel: bool) -> [bool; 2] {
+    [and(input, not(sel)), and(input, sel)]
 }
+
